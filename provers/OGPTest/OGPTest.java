cc6f1814527c29e0ff4e9b96b8c16aa93f5048b0
Nuno Baeta <nmsbaeta@gmail.com>
2021-01-28 23:45:13 +0000

    Improved code readability.

--- provers/OGPTest/OGPTest.java
+++ provers/OGPTest/OGPTest.java
@@ -13,18 +13,7 @@ public class OGPTest {
 	} else if (arguments.getVersion()) {
 	    versionMsg();
 	} else {
-	    if (arguments.getVerbose()) {
-		versionMsg();
-		System.out.println("");
-		System.out.println("   File: " + arguments.getFile());
-		System.out.println("Timeout: " + arguments.getTimeout());
-		System.out.println("Verbose: " + arguments.getVerbose());
-	    }
-	    TimeUnit.SECONDS.sleep(arguments.getTimeout());
-	    if (arguments.getVerbose()) {
-		System.out.println("");
-		System.out.println("Done!");
-	    }
+	    prove(arguments);
 	}
 	System.exit(0);
     }
@@ -42,6 +31,25 @@ public class OGPTest {
 	System.out.println("Copyright (C) 2021 Nuno Baeta");
 	System.out.println("Published under GNU GPL, version 3 or later");
 	System.out.println("https://github.com/opengeometryprover/OpenGeometryProver");
+    }
+
+    private static void prove(OGPTestArgs arguments) {
+	if (arguments.getVerbose()) {
+	    versionMsg();
+	    System.out.println("");
+	    System.out.println("   File: " + arguments.getFile());
+	    System.out.println("Timeout: " + arguments.getTimeout());
+	    System.out.println("Verbose: " + arguments.getVerbose());
+	}
+	try {
+	    TimeUnit.SECONDS.sleep(arguments.getTimeout());
+	} catch (InterruptedException e) {
+	    e.printStackTrace(System.out);
+	}
+	if (arguments.getVerbose()) {
+	    System.out.println("");
+	    System.out.println("Done!");
+	}
     }
 
 }
