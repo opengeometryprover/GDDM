bf71e15781d29f1413ccefb04c4c0494b06f2454
Nuno Baeta <nmsbaeta@gmail.com>
2021-01-30 05:47:34 +0000

    Finished verbose option behaviour.

--- provers/OGPTest/OGPTest.java
+++ provers/OGPTest/OGPTest.java
@@ -1,12 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class OGPTest {
 
     final static String MAJOR_VERSION = "0";
-    final static String MINOR_VERSION = "1";
+    final static String MINOR_VERSION = "2";
     final static String VERSION = MAJOR_VERSION + "." + MINOR_VERSION;
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)
+	throws FileNotFoundException, InterruptedException {
 	OGPTestArgs arguments = new OGPTestArgs(args);
 	if (arguments.getHelp()) {
 	    helpMsg();
@@ -16,6 +20,34 @@ public class OGPTest {
 	    prove(arguments);
 	}
 	System.exit(0);
+    }
+
+    private static void prove(OGPTestArgs arguments) {
+	if (arguments.getVerbose()) {
+	    versionMsg();
+	    System.out.println("");
+	    System.out.println("   File: " + arguments.getFile());
+	    System.out.println("Timeout: " + arguments.getTimeout());
+	    System.out.println("Verbose: " + arguments.getVerbose());
+	    System.out.println("");
+	    try {
+		Scanner scanFile = new Scanner(new File(arguments.getFile()));
+		while (scanFile.hasNextLine()) {
+		    System.out.println(scanFile.nextLine());
+		}
+	    } catch (FileNotFoundException e) {
+		e.printStackTrace(System.err);
+	    }
+	}
+	try {
+	    TimeUnit.SECONDS.sleep(arguments.getTimeout());
+	} catch (InterruptedException e) {
+	    e.printStackTrace(System.err);
+	}
+	if (arguments.getVerbose()) {
+	    System.out.println("");
+	    System.out.println("Done!");
+	}
     }
 
     private static void helpMsg() {
@@ -31,25 +63,6 @@ public class OGPTest {
 	System.out.println("Copyright (C) 2021 Nuno Baeta");
 	System.out.println("Published under GNU GPL, version 3 or later");
 	System.out.println("https://github.com/opengeometryprover/OpenGeometryProver");
-    }
-
-    private static void prove(OGPTestArgs arguments) {
-	if (arguments.getVerbose()) {
-	    versionMsg();
-	    System.out.println("");
-	    System.out.println("   File: " + arguments.getFile());
-	    System.out.println("Timeout: " + arguments.getTimeout());
-	    System.out.println("Verbose: " + arguments.getVerbose());
-	}
-	try {
-	    TimeUnit.SECONDS.sleep(arguments.getTimeout());
-	} catch (InterruptedException e) {
-	    e.printStackTrace(System.err);
-	}
-	if (arguments.getVerbose()) {
-	    System.out.println("");
-	    System.out.println("Done!");
-	}
     }
 
 }
