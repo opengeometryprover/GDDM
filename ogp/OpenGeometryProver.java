c7b3a7ab4b45f2afb080d4a0172e9b1e4cef81db
Nuno Baeta <nmsbaeta@gmail.com>
2021-02-03 02:02:42 +0000

    Arguments manipulation done by a class.

--- ogp/OpenGeometryProver.java
+++ ogp/OpenGeometryProver.java
@@ -11,84 +11,84 @@ public class OpenGeometryProver {
     final static String VERSION = MAJOR_VERSION + "." + MINOR_VERSION;
     
     public static void main(String[] args) {
-	if (args.length == 0) {
-	    System.err.print("ERROR: Incorrect number of arguments.");
-	    System.err.println(" Use option '-h' for help.");
-	    System.exit(1);
-	}
-	if (args[0].equals("-h") || args[0].equals("--help")) {
-	    System.out.println("Synopsis: ogp -h | --help");
-	    System.out.println("          ogp -p | --provers");
-	    System.out.println("          ogp -v | --version");
-	    System.out.println("          ogp file");
-	    System.out.println("          ogp file prover");
-	    System.out.println("          ogp file prover prover_options");
-	    System.exit(0);
-	}
-	if (args[0].equals("-p") || args[0].equals("--provers")) {
-	    System.out.println("This option is not yet implemented.");
-	    System.exit(0);
-	}
-	if (args[0].equals("-v") || args[0].equals("--version")) {
-	    System.out.println("OpenGeometryProver " + VERSION);
-	    System.out.println("Copyright (C) 2021 Nuno Baeta, Pedro Quaresma");
-	    System.out.println("Published under GNU GPL, version 3 or later");
-	    System.out.println("URL");
-	    System.exit(0);
-	}
-	String fileN = args[0].substring(0, args[0].lastIndexOf('.'));
-	String fileE = args[0].substring(args[0].lastIndexOf('.') + 1);
-	switch (fileE) {
-	case "coqam":
-	    System.out.println("Calling CoqAM...");
-	    break;
-	case "fof":
-	    try {
-		File conjOut = new File(fileN + "_FOF.out");
-		File conjErr = new File(fileN + "_FOF.err");
-		ArrayList<String> command = new ArrayList<String>();
-		command.add("ogptest");
-		if (args.length > 1) {
-		    for  (int i = 2; i <= args.length; i++) {
-			command.add(args[i - 1]);
+	OpenGeometryProverArgs arguments = new OpenGeometryProverArgs(args);
+	if (arguments.getHelp()) {
+	    helpMsg();
+	} else if (arguments.getVersion()) {
+	    versionMsg();
+	} else if (arguments.getProvers()) {
+	    System.out.println("[TODO] -p, --provers option not yet implemented");
+	} else {
+	    String fileN = args[0].substring(0, args[0].lastIndexOf('.'));
+	    String fileE = args[0].substring(args[0].lastIndexOf('.') + 1);
+	    switch (fileE) {
+	    case "coqam":
+		System.out.println("Calling CoqAM...");
+		break;
+	    case "fof":
+		try {
+		    File conjOut = new File(fileN + "_FOF.out");
+		    File conjErr = new File(fileN + "_FOF.err");
+		    ArrayList<String> command = new ArrayList<String>();
+		    command.add("ogptest");
+		    if (args.length > 1) {
+			for  (int i = 2; i <= args.length; i++) {
+			    command.add(args[i - 1]);
+			}
 		    }
+		    Process proc = new ProcessBuilder(command)
+			.redirectOutput(conjOut)
+			.redirectError(conjErr)
+			.start();
+		} catch(IOException e) {
+		    System.err.println("ERROR: I/O");
+		    e.printStackTrace();
 		}
-	    	Process proc = new ProcessBuilder(command)
-	    	    .redirectOutput(conjOut)
-	    	    .redirectError(conjErr)
-	    	    .start();
-	    } catch(IOException e) {
-	    	System.err.println("ERROR: I/O");
-	    	e.printStackTrace();
-	    }
-	    break;
-	case "gcl":
-	    System.out.println("Calling GCLC...");
-	    try {
-		File conjOut = new File(fileN + "_GCLC.out");
-		File conjErr = new File(fileN + "_GCLC.err");
-		ArrayList<String> command = new ArrayList<String>();
-		command.add("gclc");
-		command.add(args[0]);
-		if (args.length > 2) {
-		    for  (int i = 3; i <= args.length; i++) {
-			command.add(args[i - 1]);
+		break;
+	    case "gcl":
+		System.out.println("Calling GCLC...");
+		try {
+		    File conjOut = new File(fileN + "_GCLC.out");
+		    File conjErr = new File(fileN + "_GCLC.err");
+		    ArrayList<String> command = new ArrayList<String>();
+		    command.add("gclc");
+		    command.add(args[0]);
+		    if (args.length > 2) {
+			for  (int i = 3; i <= args.length; i++) {
+			    command.add(args[i - 1]);
+			}
 		    }
+		    System.out.println(command);
+		    Process proc = new ProcessBuilder(command)
+			.redirectOutput(conjOut)
+			.redirectError(conjErr)
+			.start();
+		} catch(IOException e) {
+		    System.err.println("ERROR: I/O");
+		    e.printStackTrace();
 		}
-		System.out.println(command);
-	    	Process proc = new ProcessBuilder(command)
-	    	    .redirectOutput(conjOut)
-	    	    .redirectError(conjErr)
-	    	    .start();
-	    } catch(IOException e) {
-	    	System.err.println("ERROR: I/O");
-	    	e.printStackTrace();
+		break;
+	    default:
+		System.out.println("ERROR: Unknown extension!");
 	    }
-	    break;
-	default:
-	    System.out.println("ERROR: Unknown extension!");
 	}
 	System.exit(0);
     }
-    
+	
+    private static void helpMsg() {
+	System.out.println("Synopsis: ogp -h | --help");
+	System.out.println("          ogp -p | --provers");
+	System.out.println("          ogp -V | --version");
+	System.out.println("          ogp file");
+	System.out.println("          ogp file prover");
+	System.out.println("          ogp file prover prover_options");
+    }
+
+    private static void versionMsg() {
+	System.out.println("OpenGeometryProver " + VERSION);
+	System.out.println("Copyright (C) 2021 Nuno Baeta, Pedro Quaresma");
+	System.out.println("Published under GNU GPL, version 3 or later");
+	System.out.println("https://github.com/opengeometryprover/OpenGeometryProver");
+    }
+
 }
---
+++ ogp/OpenGeometryProverArgs.java
@@ -0,0 +1,75 @@
+import java.io.File;
+
+public class OpenGeometryProverArgs {
+
+    private String file;
+    private boolean help = false;
+    private boolean provers = false;
+    private boolean version = false;
+
+    public OpenGeometryProverArgs(String[] args) {
+	switch (args.length) {
+	case 0:
+	    errorMsg(1, "");
+	    break;
+	case 1:
+	    help = args[0].equals("-h") || args[0].equals("--help");
+	    provers = args[0].equals("-p") || args[0].equals("--provers");
+	    version = args[0].equals("-V") || args[0].equals("--version");
+	    if (!help && !provers && !version) {
+		testFile(args[0]);
+	    }
+	    break;
+	}
+    }
+
+    public String getFile() {
+	return this.file;
+    }
+
+    public boolean getHelp() {
+	return this.help;
+    }
+
+    public boolean getProvers() {
+	return this.provers;
+    }
+
+    public boolean getVersion() {
+	return this.version;
+    }
+
+    private void testFile(String fileStr) {
+	File theFile = new File(fileStr);
+	if (!theFile.exists()) {
+	    errorMsg(2, fileStr);
+	} else if (theFile.isFile()) {
+	    errorMsg(3, fileStr);
+	} else if (!theFile.canRead()) {
+	    errorMsg(4, fileStr);
+	} else {
+	    file = fileStr;
+	}
+    }
+
+    private static void errorMsg(int error, String str) {
+	System.err.print("[ERROR] ");
+	switch (error) {
+	case 1:
+	    System.err.print("Incorrect number of arguments.");
+	    System.err.println(" Use option '-h' for help.");
+	    break;
+	case 2:
+	    System.err.println("File '" + str + "' does not exist.");
+	    break;
+	case 3:
+	    System.err.println("'" + str + "' is not a file.");
+	    break;
+	case 4:
+	    System.err.println("Cannot read file '" + str + "'.");
+	    break;
+	}
+	System.exit(error);
+    }
+
+}
