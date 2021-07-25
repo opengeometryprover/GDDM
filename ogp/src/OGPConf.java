4900e095b9bf6fd0466727c6937832190ceaa25c
Nuno Miguel dos Santos Baeta <nmsbaeta@gmail.com>
2021-07-25 22:16:11 +0100

    Configuration file parsed.

--- ogp/src/OGPConf.java
+++ ogp/src/OGPConf.java
@@ -30,18 +30,57 @@ public class OGPConf {
 		Scanner confFileScanner = new Scanner(confFile);
 		while (confFileScanner.hasNextLine()) {
 		    String line = confFileScanner.nextLine();
-		    System.out.println(line);
+
+		    int beginIndex = 0;
+		    int endIndex = line.indexOf(":", beginIndex);
+		    String id = line.substring(beginIndex, endIndex);
+		    if (id.isEmpty()) {
+			errorMsg(11, file + ": missing prover identification");
+		    }
+
+		    beginIndex = ++endIndex;
+		    endIndex = line.indexOf(":", beginIndex);
+		    String cmd = line.substring(beginIndex, endIndex);
+		    if (cmd.isEmpty()) {
+			errorMsg(11, file + ": missing prover command");
+		    }
+
+		    beginIndex = ++endIndex;
+		    endIndex = line.indexOf(":", beginIndex);
+		    String ext = line.substring(beginIndex, endIndex);
+		    if (ext.isEmpty()) {
+			errorMsg(11, file + ": missing prover extension");
+		    }
+
+		    beginIndex = ++endIndex;
+		    endIndex = line.indexOf(":", beginIndex);
+		    String toFOFCmd = line.substring(beginIndex, endIndex);
+
+		    beginIndex = ++endIndex;
+		    endIndex = line.indexOf(":", beginIndex);
+		    String toExtCmd = line.substring(beginIndex, endIndex);
+
+		    beginIndex = ++endIndex;
+		    endIndex = line.indexOf(":", beginIndex);
+		    String postProcCmd = line.substring(beginIndex, endIndex);
+
+		    String desc = line.substring(++endIndex);
 		}
 		confFileScanner.close();
 	    }
 	} catch (FileNotFoundException e) {
 	    errorMsg(99, e.toString());
+	} catch (StringIndexOutOfBoundsException e) {
+	    errorMsg(11, file + ": wrong sintax");
 	}
     }
 
     private static void errorMsg(int error, String str) {
-	System.err.print("[ERROR " + error + "] Conf : ");
+	System.err.print("[ERROR " + error + "] (OGPConf) ");
 	switch (error) {
+	case 11:
+	    System.err.println("Problem with configuration file " + str);
+	    break;
 	case 99:
 	    System.err.println("Something is really wrong :-|");
 	    System.err.println(str);
