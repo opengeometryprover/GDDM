631693a6bd647633105e713a305d1648acad2e4f
Nuno Miguel dos Santos Baeta <nmsbaeta@gmail.com>
2021-07-25 16:49:20 +0100

    This class constains information about a prover.

---
+++ ogp/src/OGPProverInfo.java
@@ -0,0 +1,44 @@
+package ogp;
+
+public class OGPProverInfo {
+
+    private String id;		// Prover's name
+    private String cmd;		// Command line command
+    private String ext;		// Conjecture's extension
+    private String toFOFCmd;	// ext2FOF command, if any
+    private String toExtCmd;	// FOF2ext command, if any
+    private String postProcCmd;	// Post-processing command, if any
+    private String desc;	// Prover's description
+
+    public OGPProverInfo() {
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public void setCmd(String cmd) {
+	this.cmd = cmd;
+    }
+
+    public void setExt(String ext) {
+	this.ext = ext;
+    }
+
+    public void setToFOFCmd(String toFOFCmd) {
+	this.toFOFCmd = toFOFCmd;
+    }
+
+    public void setToExtCmd(String toExtCmd) {
+	this.toExtCmd = toExtCmd;
+    }
+
+    public void setPostProcCmd(String postProcCmd) {
+	this.postProcCmd = postProcCmd;
+    }
+
+    public void setDesc(String desc) {
+	this.desc = desc;
+    }
+
+}
