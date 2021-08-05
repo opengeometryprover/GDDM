a1f2e7587ca2be981fb0da421a0f0bb8bab3fa55
Nuno Miguel dos Santos Baeta <nmsbaeta@gmail.com>
2021-08-05 20:39:47 +0100

    File printing done.

--- provers/ogptest/src/OGPTest.java
+++ provers/ogptest/src/OGPTest.java
@@ -1,9 +1,12 @@
 package ogptest;
 
-// import java.io.File;
 // import java.io.FileNotFoundException;;
 // import java.util.concurrent.TimeUnit;
 // import java.util.Scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class OGPTest {
 
@@ -22,7 +25,19 @@ public class OGPTest {
     }
 
     private static void prove(OGPTestArgs arguments) {
-	// TODO
+	try {
+	    System.out.println("File: " + arguments.getFile());
+	    File theFile = new File(arguments.getFile());
+	    Scanner theFileScanner = new Scanner(theFile);
+	    while (theFileScanner.hasNextLine()) {
+		System.out.println("    " + theFileScanner.nextLine());
+	    }
+	    System.out.println();
+	} catch (FileNotFoundException e) {
+	    errorMsg(999, e.toString());
+	}
+	System.out.print("Timeout: " + arguments.getTimeout() + "...");
+	System.out.println(" Done!!!");
     }
 
     private static void helpMsg() {
@@ -39,6 +54,16 @@ public class OGPTest {
 	System.out.println("Copyright (C) 2021 Nuno Baeta");
 	System.out.println("Published under GNU GPL, version 3 or later");
 	System.out.println("https://github.com/opengeometryprover/OpenGeometryProver");
+    }
+
+    private static void errorMsg(int error, String msg) {
+	System.err.print("[OGPTest ERROR " + error + "] (OGPTest) ");
+	switch (error) {
+	case 999:
+	    System.err.println("Something is really wront :-|");
+	    System.err.println(msg);
+	    break;
+	}
     }
 
 }
