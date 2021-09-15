b622f4c2c0024306a3ccdf78a8ecd24432746eb3
Nuno Miguel dos Santos Baeta <nmsbaeta@gmail.com>
2021-09-15 20:31:24 +0100

    Updated proved reasons.

--- postProcessing/ppVampire/ppVampire.sh
+++ postProcessing/ppVampire/ppVampire.sh
@@ -3,6 +3,9 @@
 if grep -q 'Termination reason: Refutation' "$1".out
 then
     echo Proved > "$1".result
+elif grep -q 'Termination reason: Satisfiable' "$1".out
+then
+    echo Proved > "$1".result
 else
     echo Unkown > "$1".result
 fi
