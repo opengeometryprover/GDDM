9b581e49bcf2a02cbade06980e2c20911f904759
Nuno Migue dos Santos Baeta <nmsbaeta@gmail.com>
2022-08-16 03:24:08 +0100

    Rule D61: rule and its use verified.

--- provers/ogpgddm/src/prover.cpp
+++ provers/ogpgddm/src/prover.cpp
@@ -7380,7 +7380,7 @@ DBinMemory Prover::fixedPoint(DBinMemory dbim) {
 		dbim = ruleD56(dbim, point1, point2, point3, point4);
 	    if (point2 == point4)
 		dbim = ruleD57cong(dbim, point1, point2, point3, point4);
-	    // dbim = ruleD61cong(dbim, point1, point2, point3, point4);
+	    dbim = ruleD61cong(dbim, point1, point2, point3, point4);
 	    // if (point1 == point3)
 	    //  dbim = ruleD67cong(dbim, point1, point2, point3, point4);
 	    // dbim = ruleD75cong(dbim, point1, point2, point3, point4);
@@ -7502,8 +7502,8 @@ DBinMemory Prover::fixedPoint(DBinMemory dbim) {
 			   point4, point5, point6);
 	    dbim = ruleD60(dbim, point1, point2, point3,
 			   point4, point5, point6);
-	    // dbim = ruleD61(dbim, point1, point2, point3,
-	    // 		   point4, point5, point6);
+	    dbim = ruleD61simtri(dbim, point1, point2, point3,
+				 point4, point5, point6);
 	    break;
 	default:
             // ERROR : Necessary?  I don't _thin_ so...
