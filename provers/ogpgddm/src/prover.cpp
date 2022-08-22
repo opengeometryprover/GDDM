dee7c7cad090e98aae340525758669019396ca4d
Nuno Migue dos Santos Baeta <nmsbaeta@gmail.com>
2022-08-23 00:51:54 +0100

    Correction to rule D64 (para).

--- provers/ogpgddm/src/prover.cpp
+++ provers/ogpgddm/src/prover.cpp
@@ -5663,8 +5663,7 @@ DBinMemory Prover::ruleD64para(DBinMemory dbim, std::string point1,
 	    "FROM NewFact "
 	    "INNER JOIN Midpoint "
 	    "ON (newFact = id) "
-	    "WHERE point2 = '" + point1 + "' AND point3 = '" + point3
-	    + "' AND point1 NOT IN ('" + point2 + "', '" + point4 + "')";
+	    "WHERE point2 = '" + point1 + "' AND point3 = '" + point3;
 	dbim.rc = sqlite3_prepare_v2(dbim.db, querySecondGeoCmdA.c_str(),
 				     querySecondGeoCmdA.size(), &(dbim.stmt1),
 				     NULL);
@@ -5673,8 +5672,7 @@ DBinMemory Prover::ruleD64para(DBinMemory dbim, std::string point1,
 	    "FROM Facts "
 	    "INNER JOIN Midpoint "
 	    "ON (oldFact = id) "
-	    "WHERE point2 = '" + point1 + "' AND point3 = '" + point3
-	    + "' AND point1 NOT IN ('" + point2 + "', '" + point4 + "')";
+	    "WHERE point2 = '" + point1 + "' AND point3 = '" + point3;
 	dbim.rc = sqlite3_prepare_v2(dbim.db, querySecondGeoCmdB.c_str(),
 				     querySecondGeoCmdB.size(), &(dbim.stmt2),
 				     NULL);
