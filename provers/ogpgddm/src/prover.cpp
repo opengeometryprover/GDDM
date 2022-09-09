4473f51bbdbea10555ccea116cedee636b0bd97e
Nuno Migue dos Santos Baeta <nmsbaeta@gmail.com>
2022-09-09 21:26:43 +0100

    Derivation of new facts: parallel command verified.

--- provers/ogpgddm/src/prover.cpp
+++ provers/ogpgddm/src/prover.cpp
@@ -7489,18 +7489,17 @@ DBinMemory Prover::fixedPoint(DBinMemory dbim) {
 	    dbim = ruleD05(dbim, point1, point2, point3, point4);
 	    dbim = ruleD06(dbim, point1, point2, point3, point4);
 	    dbim = ruleD10para(dbim, point1, point2, point3, point4);
-	    if (point1 != point2 && point1 != point3 && point1 != point4
-		&& point2 != point3 && point2 != point4 && point3 != point4) {
-		dbim = ruleD40(dbim, point1, point2, point3, point4);
+	    dbim = ruleD40(dbim, point1, point2, point3, point4);
+	    if (point1 != point3 && point2 != point4)
 		dbim = ruleD45para(dbim, point1, point2, point3, point4);
-		dbim = ruleD54para(dbim, point1, point2, point3, point4);
+	    dbim = ruleD54para(dbim, point1, point2, point3, point4);
+	    if (point1 != point3 && point2 != point4)
 		dbim = ruleD64para(dbim, point1, point2, point3, point4);
+	    if (point1 != point3 && point2 != point4)
 		dbim = ruleD65para(dbim, point1, point2, point3, point4);
-		dbim = ruleD73para(dbim, point1, point2, point3, point4);
-	    }
-	    if (point1 == point3
-		&& point1 != point2 && point1 != point4 && point2 != point4)
+	    if (point1 == point3)
 		dbim = ruleD66(dbim, point1, point2, point3, point4);
+	    dbim = ruleD73para(dbim, point1, point2, point3, point4);
 	    break;
 	case 3:
             // Perpendicular
