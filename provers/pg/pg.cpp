c96f8add443e94e5f10bf8d8fe3aa6598f82aa52
Nuno Miguel dos Santos Baeta <nmsbaeta@gmail.com>
2025-04-17 09:32:15 +0100

    PG's main files.

---
+++ provers/pg/pg.cpp
@@ -0,0 +1,76 @@
+/*
+ * pg.cpp
+ *
+ * Given a set of geometric axioms in the first order form (FOF), as described
+ * in TPTP, generates the corresponding C++ code of a GDDM prover.
+ *
+ * This file is part of 'pg', the OGP Provers-Generator (PG) for the Geometry
+ * Deductive Database Method (GDDM).
+ *
+ * Copyright (C) 2025 Nuno, Pedro Quaresma
+ * Distributed under GNU GPL 3.0 or later.
+ */
+
+
+#include <cstring>
+#include <iostream>
+#include <list>
+#include <string>
+
+#include "parser.h"
+
+#include "pg.hpp"
+#include "axioms.hpp"
+#include "generate.hpp"
+#include "warnerr.hpp"
+
+Axiom_list axioms;
+
+extern FILE *yyin;
+
+/*
+ * Given a set of geometric axioms in the first order form, as described
+ * in TPTP, builds the corresponding C++ code.
+ */
+int main(int argc, char *argv[])
+{
+    std::string gentxt;
+    FILE *outfile;
+
+    if (argc < 2) {
+	error(ERROR_MISSING_ARGUMENT, "");
+    } else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+	std::cout << PG_ID;
+	std::cout << std::endl;
+	std::cout << "Usage:" << std::endl;
+	std::cout << "\tpg [option | axioms]" << std::endl;
+	std::cout << "where option is one of:" << std::endl;
+	std::cout << "\t-h | --help\tthis help" << std::endl;
+	std::cout << "\t-V | --version\tbuilder's version" << std::endl;
+	std::cout << "and" << std::endl;
+	std::cout << "\taxioms\t\tfile with the list of axioms" << std::endl;
+    } else if (!strcmp(argv[1], "-V") || !strcmp(argv[1], "--version")) {
+	std::cout << PG_ID;
+    } else {
+	if (argc > 2)
+	    warning(WARNING_EXCESS_ARGUMENTS, "");
+	// Parse axiom file
+	if ((yyin = fopen(argv[1], "r")) == NULL)
+	    error(ERROR_UNABLE_OPEN_FILE_READ, argv[1]);
+	yyparse();
+	fclose(yyin);
+	// Generate 'prover.hpp'
+	gentxt = generate_prover_hpp();
+	if ((outfile = fopen("prover.hpp", "w")) == NULL)
+	    error(ERROR_UNABLE_OPEN_FILE_WRITE, "prover.hpp");
+	fprintf(outfile, "%s", gentxt.c_str());
+	fclose(outfile);
+	// Generate 'prover.cpp'
+	gentxt = generate_prover_cpp();
+	if ((outfile = fopen("prover.cpp", "w")) == NULL)
+	    error(ERROR_UNABLE_OPEN_FILE_WRITE, "prover.cpp");
+	fprintf(outfile, "%s", gentxt.c_str());
+	fclose(outfile);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ provers/pg/pg.hpp
@@ -0,0 +1,25 @@
+/*
+ * pg.hpp
+ *
+ * Macro definitions.
+ *
+ * This file is part of 'pg', the OGP Provers-Generator (PG) for the Geometry
+ * Deductive Database Method (GDDM).
+ *
+ * Copyright (C) 2025 Nuno, Pedro Quaresma
+ * Distributed under GNU GPL 3.0 or later.
+ */
+
+#ifndef PG
+#define PG
+
+#define VERSION "0.0.1"
+#define YEAR "2025"
+#define AUTHORS "Nuno Baeta, Pedro Quaresma"
+#define LICENSE "GNU GPL 3.0 or later"
+
+#define PG_ID "pg " VERSION "\n"      \
+	"Copyright (C) " YEAR " " AUTHORS "\n" \
+	"Distributed under " LICENSE "\n"
+
+#endif
